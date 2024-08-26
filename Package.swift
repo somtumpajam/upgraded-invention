95be6247a30bdc9412b87a316fc7ee77b3500633
ส้มตำป้าแจ่ม <viriyasupantuchaikul@gmail.com>
2024-08-27 02:12:12 +0700

    Initialize releases/v16.2.0

---
+++ Package.swift
@@ -0,0 +1,239 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import PackageDescription
+import Foundation
+
+#if os(Linux)
+import Glibc
+#else
+import Darwin.C
+#endif
+
+let package = Package(
+    name: "Facebook",
+    platforms: [.iOS(.v12)],
+    products: [
+        // The Kernel of the SDK. Must be included as a runtime dependency.
+        .basics,
+
+        // The Facebook AEM Kit
+        .aem,
+
+        /*
+          The Core SDK library that provides two importable modules:
+
+            - FacebookCore which includes the most current interface and
+              will contain interfaces for new features written in Swift
+
+            - FBSDKCoreKit which contains legacy Objective-C interfaces
+              that will be used to maintain backwards compatibility with
+              types that have been converted to Swift.
+              This will not contain interfaces for new features written in Swift.
+         */
+        .core,
+
+        // The Facebook Login SDK
+        .login,
+
+        // The Facebook Share SDK
+        .share,
+
+        // The Facebook Gaming Services SDK
+        .gaming,
+    ],
+    targets: [
+        // The kernel of the SDK
+        .Prefixed.basics,
+
+        /*
+          The legacy Objective-C implementation that will be converted to Swift.
+          This will not contain interfaces for new features written in Swift.
+        */
+        .Prefixed.aem,
+
+        // The main AEM module
+        .aem,
+
+        /*
+          The legacy Objective-C implementation that will be converted to Swift.
+          This will not contain interfaces for new features written in Swift.
+        */
+        .Prefixed.core,
+
+        // The main Core SDK module
+        .core,
+
+        /*
+          The legacy Objective-C implementation that will be converted to Swift.
+          This will not contain interfaces for new features written in Swift.
+        */
+        .Prefixed.login,
+
+        // The main Login SDK module
+        .login,
+
+        /*
+          The legacy Objective-C implementation that has been converted to Swift.
+          This will not contain interfaces for new features written in Swift.
+        */
+        .Prefixed.share,
+
+        // The main Share SDK module
+        .share,
+
+        /*
+          The legacy Objective-C implementation that has been converted to Swift.
+          This will not contain interfaces for new features written in Swift.
+        */
+        .Prefixed.gamingServices,
+
+        // The main Facebook Gaming Services module
+        .gaming,
+    ],
+    cxxLanguageStandard: .cxx11
+)
+
+extension Product {
+    static let basics = library(name: .basics, targets: [.Prefixed.basics])
+    static let core = library(name: .core, targets: [.core, .Prefixed.core])
+    static let login = library(name: .login, targets: [.login])
+    static let share = library(name: .share, targets: [.share, .Prefixed.share])
+    static let gaming = library(name: .gaming, targets: [.gaming, .Prefixed.gaming])
+    static let aem = library(name: .aem, targets: [.aem, .Prefixed.aem])
+}
+
+extension Target {
+    static let binarySource = BinarySource()
+
+    static func binaryTarget(name: String, remoteChecksum: String) -> Target {
+        switch binarySource {
+        case .local:
+            return .binaryTarget(
+                name: name,
+                path: localBinaryPath(for: name)
+            )
+        case .remote:
+            return .binaryTarget(
+                name: name,
+                url: remoteBinaryURLString(for: name),
+                checksum: remoteChecksum
+            )
+        }
+    }
+
+    static func localBinaryPath(for targetName: String) -> String {
+        "build/XCFrameworks/Static/\(targetName).xcframework"
+    }
+
+    static func remoteBinaryURLString(for targetName: String) -> String {
+        "https://github.com/facebook/facebook-ios-sdk/releases/download/v16.1.3/\(targetName)-Static_XCFramework.zip"
+    }
+
+    static let aem = target(name: .aem, dependencies: [.Prefixed.aem])
+
+    static let core = target(
+        name: .core,
+        dependencies: [.aem, .Prefixed.basics, .Prefixed.core],
+        linkerSettings: [
+            .cPlusPlusLibrary,
+            .zLibrary,
+            .accelerateFramework,
+        ]
+    )
+
+    static let login = target(name: .login, dependencies: [.core, .Prefixed.login])
+
+    static let share = target(name: .share, dependencies: [.core, .Prefixed.share])
+
+    static let gaming = target(name: .gaming, dependencies: [.Prefixed.gaming])
+
+    enum Prefixed {
+        static let basics = binaryTarget(
+            name: .Prefixed.basics,
+            remoteChecksum: "eadaa16330035fcf06f71db7b89312b9d020f947fbde0bb55446a28bc59ca18c"
+        )
+
+        static let aem = binaryTarget(
+            name: .Prefixed.aem,
+            remoteChecksum: "8c2460b6dd2834aad78b40a2e6b51316c6489d4f84c975794a53e3dfb5841a0a"
+        )
+
+        static let core = binaryTarget(
+            name: .Prefixed.core,
+            remoteChecksum: "47a23bbc85881781505d871ee9f43790bf1e57942081cbeea96689fc86c22dac"
+        )
+
+        static let login = binaryTarget(
+            name: .Prefixed.login,
+            remoteChecksum: "5ff84e9262a1a7fbe84ae9b32deeaeb0e6ec1aa2c68ac9cc7a39e85147053569"
+        )
+
+        static let share = binaryTarget(
+            name: .Prefixed.share,
+            remoteChecksum: "bf0e61b865d948725f67a87bdcba7e445373d9aab8bf9c27f592bd101107aba6"
+        )
+
+        static let gamingServices = binaryTarget(
+            name: .Prefixed.gaming,
+            remoteChecksum: "3d8460ca8d0011066835cd02fff9510e8c4d496c4f0e3597cad7f4d7ec490d2e"
+        )
+    }
+}
+
+extension Target.Dependency {
+    static let aem = byName(name: .aem)
+    static let core = byName(name: .core)
+
+    enum Prefixed {
+        static let aem = byName(name: .Prefixed.aem)
+        static let basics = byName(name: .Prefixed.basics)
+        static let core = byName(name: .Prefixed.core)
+        static let login = byName(name: .Prefixed.login)
+        static let share = byName(name: .Prefixed.share)
+        static let gaming = byName(name: .Prefixed.gaming)
+    }
+}
+
+extension LinkerSetting {
+    static let cPlusPlusLibrary = linkedLibrary("c++")
+    static let zLibrary = linkedLibrary("z")
+    static let accelerateFramework = linkedFramework("Accelerate")
+}
+
+enum BinarySource {
+    case local, remote
+
+    init() {
+        if getenv("USE_LOCAL_FB_BINARIES") != nil {
+            self = .local
+        } else {
+            self = .remote
+        }
+    }
+}
+
+extension String {
+    static let aem = "FacebookAEM"
+    static let basics = "FacebookBasics"
+    static let core = "FacebookCore"
+    static let login = "FacebookLogin"
+    static let share = "FacebookShare"
+    static let gaming = "FacebookGamingServices"
+
+    enum Prefixed {
+        static let aem = "FBAEMKit"
+        static let basics = "FBSDKCoreKit_Basics"
+        static let core = "FBSDKCoreKit"
+        static let login = "FBSDKLoginKit"
+        static let share = "FBSDKShareKit"
+        static let gaming = "FBSDKGamingServicesKit"
+    }
+}
